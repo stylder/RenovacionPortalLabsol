4287d72cedf4459138ffed35eb7e21ddc707236b
stylder <Santiago1>
2017-09-30 23:48:23 -0500

    RESPALDO SQL

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -160,30 +160,6 @@
                                     <a  href="mailto:contacto@cozcyt.gob.mx " target="_blank">Contacto
                                     </a>
                                 </li>
-
-                                <li class="dropdown">
-                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="#">2-level Dropdown <i class="icon-arrow-right"></i></a>
-                                            <ul class="dropdown-menu sub-menu">
-                                                <li><a href="#">Action</a></li>
-                                                <li><a href="#">Another action</a></li>
-                                                <li><a href="#">Something else here</a></li>
-                                                <li class="divider"></li>
-                                                <li class="nav-header">Nav header</li>
-                                                <li><a href="#">Separated link</a></li>
-                                                <li><a href="#">One more separated link</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Another action</a></li>
-                                        <li><a href="#">Something else here</a></li>
-                                        <li class="divider"></li>
-                                        <li class="nav-header">Nav header</li>
-                                        <li><a href="#">Separated link</a></li>
-                                        <li><a href="#">One more separated link</a></li>
-                                    </ul>
-                                </li>
                             </ul>
 
                         </div>
